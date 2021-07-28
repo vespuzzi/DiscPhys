4efa444b20590ca23d139e9994805523791ee54c
kilvesq <vesa.kilpi@sitowise.com>
2021-07-28 14:47:34 +0300

    Default pitching moment, refactoring coeff data

--- src/model/Disc.ts
+++ src/model/Disc.ts
@@ -42,16 +42,22 @@ const defaultLiftArgs = [-90, -45, -10, -5,  0,  5,   10, 15, 20,  25, 30,  40, 
 const defaultLiftVals = [0.0, -0.9,-.25, 0, .2, 0.4, .6, .9,  1.2, 1.4, 1.7, 0.9, 1.0 ,0.5 , 0]; 
 const defaultLiftCoef = new Spline(defaultLiftArgs, defaultLiftVals);
 
-const defaultDragArgs = [-15, -10, -5,  0,  5,   10, 15, 20,  25,  30,  40, 50, 90];
-const defaultDragVals = [.17, .15, .19, .1, .15, .29, .4,  .59, .72, .9, 1.01, 1.08, 1.4]; 
-const defaultDragCoef = new Spline(defaultLiftArgs, defaultLiftVals);
+const defaultDragArgs = [-90, -45, -15, -10, -5,  0,  5,   10, 15, 20,  25,  30,  40, 50, 90];
+const defaultDragVals = [1.3, 0.9, .17, .15, .19, .1, .15, .29, .4,  .59, .72, .9, 1.01, 1.08, 1.4]; 
+const defaultDragCoef = new Spline(defaultDragArgs, defaultDragVals);
+
+const defaultPitchingArgs = [-90.   -15,   -10,   -5,    0,     5,  10,    15,    20,    25,   30,    40,  50,    90];
+const defaultPitchingVals = [-0.01, -.065, -.05, -.035, -0.02, .01, 0.001, 0.025, 0.045, 0.07, 0.11,  0.17, 0.19, 0.01]; 
+const defaultPitchingCoef = new Spline(defaultPitchingArgs, defaultPitchingVals);
+
 
 export class DiscProps {
 	readonly m: number; //mass
 	readonly d: number; //disc diameter
 	readonly rm: number; // mass radius: -> moment of inertia
-	readonly cLift: any;
-	readonly cDrag: any;
+	readonly cLift: any;  //Lift coefficient as function of angle of attack in degrees implementeed as cubic spline 
+	readonly cDrag: any;  //Drag coefficient as function of angle of attack in degrees implementeed as cubic spline 
+	readonly cPitching: any; //Pitching moment coefficient as function of angle in degrees implementeed as cubic spline 
 	// double A;
 	// double I; //moment of inertia
 	constructor (
@@ -59,19 +65,33 @@ export class DiscProps {
 		d: number = 0.21,
 		rm: number = 0.9 * d/2,
 		cLift: any = defaultLiftCoef,
-		cDrag: any = defaultDragCoef
+		cDrag: any = defaultDragCoef,
+		cPitching: any = defaultPitchingCoef
 	) {
 		this.m = m;
 		this.d = d
 		this.rm = rm;
 		this.cLift = cLift;
 		this.cDrag = cDrag;
+		this.cPitching = cPitching;
 	}
 
 	cLiftData(start: number, end: number, step: number): any {
+		return this.coeffData(start, end, step, this.cLift);
+	} 
+
+	cDragData(start: number, end: number, step: number): any {
+		return this.coeffData(start, end, step, this.cDrag);
+	}
+
+	cPitchingData(start: number, end: number, step: number): any {
+		return this.coeffData(start, end, step, this.cPitching);
+	}
+
+    coeffData(start: number, end: number, step: number, spline: any): any {
 		const data = [];
 		for (var x = start; x <= end; x+= step) {
-			data.push({x, y: this.cLift.at(x)});
+			data.push({x, y: spline.at(x)});
 		}
 		return data;
 	} 
