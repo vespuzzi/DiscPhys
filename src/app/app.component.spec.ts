7855137d46ad5246a498b80fde6e8a50788e1080
kilvesq <vesa.kilpi@sitowise.com>
2021-09-08 09:37:44 +0300

    muutoksia

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,6 +13,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    console.log(app)
     expect(app).toBeTruthy();
   });
 
--- tests/Disc.test.ts
+++ tests/Disc.test.ts
@@ -1,8 +1,13 @@
-import { DiscProps } from '../src/model/Disc'
+import { DiscProps, DiscState } from '../src/model/Disc'
 
 test ('lift coeff', () => {
     const disc = new DiscProps();
     console.log(disc.cLift.at(-5));
     console.log(disc.cLift.at(-2));
     console.log(disc.cLift.at(-80));
+  })
+
+  test('DiscProps', () => {
+    const dProps = new DiscProps();
+    const dState = new DiscState()
   })
