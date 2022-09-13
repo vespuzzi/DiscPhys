fb4b8981f9141cd9a95950cf62568a553632c0ca
Vesa Kilpi <vespuzzi@gmail.com>
2022-09-13 10:47:31 +0300

    Minor tweaks

--- src/model/Disc.ts
+++ src/model/Disc.ts
@@ -7,13 +7,12 @@ export class DiscState {
     readonly r: Vector3; //position
 	readonly v: Vector3; //velocity
 	readonly w: Vector3; //angular velocity
-	readonly n: Vector3; //orientation:  unit vector perpendicular to disc plane 
+	readonly n: Vector3; //orientation:  unit vector perpendicular to disc plane pointig to the direction of the disc dome
     constructor (
 			r: Vector3 = new Vector3(0,0,1.5), //default initial height = 1.5
 			v: Vector3 = new Vector3(0,0,0),
 			spin: number = 1,           //revolutions/second, positive sign -> counterclockwise rotation
 			n: Vector3 = new Vector3(0,0,1),
-
 		) {
 			this.r = r;
 			this.v = v;
@@ -22,14 +21,14 @@ export class DiscState {
 	}
 }
 
-const defaultLiftArgs = [-90, -45, -10,  -5,    -2.5,  0,    5,   10, 15,   20,   25, 30,  40,   45, 67, 90];
-const defaultLiftVals = [0.0, -0.75,-.25, -0.09, 0,   0.14, 0.45, .75, 1,  1.25, 1.5, 1.7, 0.9, 1.0 ,0.5 , 0]; 
+const defaultLiftArgs = [-90, -45, -10,  -5,    -2.5,  0,    5,   10, 15,   20,   25, 30,  40, 45, 64, 90];
+const defaultLiftVals = [0.0, -0.75,-.25, -0.09, 0,   0.14, 0.45, .75, 1,  1.25, 1.5, 1.7, 1.3, 0.9 ,0.3 , 0]; 
 const defaultLiftCoef = new Spline(defaultLiftArgs, defaultLiftVals);
 
 const defaultDragArgs = [-90, -45, -20,   -15, -10, -5,  0,    5,   10, 15, 20,  25,  30,   40,    50, 90];
 const defaultDragVals = [1.9, 0.9, 0.38,  .28, .18, .11, 0.1, .15, .27, .4, 0.58, .72, 0.9, 1.3, 1.7, 2.1]; 
 const defaultDragCoef = new Spline(defaultDragArgs, defaultDragVals);
-
+	 
 const defaultPitchingArgs = [-90.   -15,   -10,   -5,    0,     5,  10,    15,    20,    25,   30,    40,  50,    90];
 const defaultPitchingVals = [-0.01, -.065, -.04, -.035, -0.02, .01, 0.001, 0.025, 0.045, 0.07, 0.11,  0.17, 0.19, 0.01]; 
 const defaultPitchingCoef = new Spline(defaultPitchingArgs, defaultPitchingVals);
