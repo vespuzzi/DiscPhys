c1e184eca792ca77be47ef3968c5f75904956480
Vesa Kilpi <vespuzzi@gmail.com>
2022-10-01 02:49:56 +0300

    UI text tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { SimulationChartComponent } from './simulation-chart/simulation-chart.co
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Disc Physics Simulator';
+  title = 'Disc Physics Simulator 0.1 Beta';
   discSetup: DiscSetup = {releaseHeight: 1.4, speed: 30, spin: 25, pitchAngle: 11, tilt: 0, initialAoA: 4};
   
   updateSimulationInitialConditions(simulationChart: SimulationChartComponent, event: any, prop: string){
