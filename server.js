bd365e1c5fc390a3b63638ca3cba8b112b1d51d7
Vesa Kilpi <vespuzzi@gmail.com>
2022-10-01 00:49:23 +0300

    fix server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/disc-phys'));
+app.use(express.static(__dirname + '/dist/DiscPhys'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/DiscPhys/index.html'));});
 app.listen(process.env.PORT || 8080);
