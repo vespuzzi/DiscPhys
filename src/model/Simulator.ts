1fe1671743ea36d4859bd2c0dc6801ae5b90eafa
Vesa Kilpi <vespuzzi@gmail.com>
2022-10-05 00:50:52 +0300

    simulation setup refactoring

--- src/model/Simulator.ts
+++ src/model/Simulator.ts
@@ -20,30 +20,56 @@ export class Environment{
 /**
  * Simulation setup contains initial values for simulation in "human understandable form"
  */
-export class SimulationSetup{
-    [name: string]: number;
-    releaseHeight = 1.4; 
-    speed = 100;
-    spin = -25; 
-    pitchAngle = 10; 
-    tilt = 0;  
-    initialAoA = 0; 
-    airDensity = 1.2;
-    windSpeed = 0;
-    windDirection = 0; //0 = tailwind, 
-    windUpdraft = 0;
-    discMass = 180;  //g
-    discDiameter = 21; //cm
-    discMassRadius = 9 //cm
+
+export const defaultSimulationSetup = {
+    releaseHeight: {value: 1.4, prompt: 'Release height',                   
+                     unit: 'm',     min: 0.5,  max: 2.5, step: 0.1, category: 'discState',
+                     tooltip: 'Distance from ground in the beginning'}, 
+    speed:          {value: 100, prompt: 'Speed',                            
+                     unit: 'km/h',   min: 5,  max: 160, step: 1, category: 'discState',
+                     tooltip: "Magnitude of the velocity in the beginning"},
+    spin:           {value: -25, prompt: 'Spin: right handed bh < 0 < fore', 
+                     unit: 'Hz',   min: -40,  max: 40, step: 1, category: 'discState',
+                     tooltip: 'Disc rotation speed as revolutions per second. Positive direction is counterclockwise (right hand curl rule)'},
+    pitchAngle:     {value: 5,   prompt: 'Pitch angle',                      
+                     unit: '°',   min: -10,  max: 90, step: 0.5, category: 'discState',
+                     tooltip: 'Initial direction of the disc velocity. 0° means horizontal'},
+    initialAoA:     {value: 0,   prompt: 'Initial angle of attack',          
+                     unit: '°',   min: -45,  max: 90, step: 0.5, category: 'discState',
+                     tooltip: 'Angle between disc plane and disc velocity'}, 
+    tilt:           {value: 0,   prompt: 'Tilt: left < 0 < right',           
+                     unit: '°',   min: -180,  max: 180, step: 0.5, category: 'discState',
+                     tooltip: 'Initial sideways tilt of the disc. Positive values mean tilt to the right (right hand curl rule when rotating disc normal vector about velocity vector) '},
+    airDensity:     {value: 1.2, prompt: 'Air density: ',                    
+                     unit: 'kg/m^3', min: 0.5,  max: 1.5, step: 0.01, category: 'discState', 
+                     tooltip: 'Air density varies with altitude and temperature. At sea level 30°C -> 1.16kg/m^3, -25°C -> 1.422kg/m^3'},
+    windSpeed:      {value: 0,   prompt: 'Wind speed',                       
+                     unit: 'm/s',  min: 0.5,  max: 2.5, step: 0.1, category: 'environment',
+                     tooltip: 'Magnitude of wind velocity in horizontal plane'},
+    windDirection:  {value: 0,   prompt: 'Wind direction: from left < 0 < from right',           
+                     unit: '°',   min: -180,  max: 180, step: 0.5, category: 'environment',
+                     tooltip: 'Direction of wind: positive values mean wind blowing from right. Absolute values < 90 mean tailwind'},
+    windUpdraft:    {value: 0, prompt: 'Wind updraft',                            
+                     unit: 'm/s',   min: -5,  max: 5, step: 0.1, category: 'environment',
+                     tooltip: 'Vertical component of wind. Positive values mean updraft'},
+    discMass:       {value: 180, prompt: 'Disc mass',                            
+                     unit: 'g',   min: 10,  max: 250, step: 1, category: 'discProps',
+                     tooltip: 'Disc mass in grams. Maximum PDGA approved weight is 200g'},
+    discDiameter:   {value: 21, prompt: 'Disc diameter',                            
+                     unit: 'cm',   min: 5,  max: 40, step: 0.2, category: 'discProps',
+                     tooltip: 'Minimum PDGA approved diameter is 21cm'},
+    discMassRadius:  {value: 10.5, prompt: 'Disc mass radius',                            
+                      unit: 'cm', min: 1,  max: 10.5, step: 0.1, category: 'discProps',
+                      tooltip: 'Radius of a ring that has the same moment of inertia as the disc (Roc3 has 9cm mass radius, Star Boss 8.4cm )'},
 }
 
 export class Simulation {
     readonly A: number              //   disc area
     readonly I: number              //   moment of inertia along principal axis
-    readonly env: Environment;
+    env: Environment;
     readonly discProps: DiscProps;
     spinDirection: number | undefined;
-    curL: Vector3 | undefined;
+    curL: Vector3 | undefined; 
 
     constructor (discProps: DiscProps, env: Environment = new Environment()){
         const discRadius = discProps.d/2; 
@@ -54,12 +80,16 @@ export class Simulation {
         this.discProps = discProps;
 
     }
-
+    
+    randomPerturbance(magnitude:number= 0.0001): Vector3 {
+        return new Vector3(Math.random()*magnitude, Math.random()*magnitude, Math.random()*magnitude);
+    }
 
     updateDiscState(discState: DiscState, dt: number) : DiscState{
         //Spin direction: if disc normal vector and angular speed direction are same spin direction is positive ie. counterclockwise otherwise clockwise
         this.spinDirection = this.spinDirection ? this.spinDirection : Math.sign(discState.w.dot(discState.n))
         const va = discState.v.sub(this.env.windVelocity);           //Disc air velocity
+        this.env.windVelocity = this.env.windVelocity.sum(this.randomPerturbance())
         const Va = va.length();                                      //Disc air speed
         const q = .5 * this.A * this.env.rho * Va * Va;              //Dynamic pressure; 
         const aoa = rad2Deg(va.angle(discState.n)-Math.PI/2);        //angle of attack in degrees
@@ -121,7 +151,7 @@ export function deg2Rad(deg: number)
  * @param speed         Magnitude of the velocity of the disc in meters per second
  * @param spin          Rotation speed of the disc in revolutions per second
  * @param pitchAngle    Direction of the disc motion in degrees: larger value means more upwards  from xy-plane
- * @param anhyzer       Tilt of the disc in degrees larger value means more tilt to the left. Negative values mean hyzer
+ * @param tilt          Tilt of the disc in degrees larger value means more tilt to the right. 
  * @param initialAoA    Initial angle of attack in degrees: Angle between disc plane and disc velocity
  * @return              Initial disc state
  */
@@ -138,23 +168,23 @@ export function getInitialDiscState(
   return new DiscState(position, velocity, spin, ori)
 }
 
-export function initializeDiscState(setup: SimulationSetup):DiscState{
+export function initializeDiscState(setup: any):DiscState{
     return getInitialDiscState(
-              setup.releaseHeight,
-              setup.speed,
-              setup.spin,
-              setup.pitchAngle,
-              setup.tilt,
-              setup.initialAoA
+              setup.releaseHeight.value,
+              setup.speed.value,
+              setup.spin.value,
+              setup.pitchAngle.value,
+              setup.tilt.value,
+              setup.initialAoA.value
               );
 }
-export function initializeEnvironment(setup: SimulationSetup): Environment{
+export function initializeEnvironment(setup: any): Environment{
     return new Environment(
-        setup.airDensity, 
+        setup.airDensity.value, 
         new Vector3(
-            setup.windSpeed*Math.cos(deg2Rad(setup.windDirection)),
-            setup.windSpeed*Math.sin(deg2Rad(setup.windDirection)),
-            setup.windUpdraft),
+            setup.windSpeed.value*Math.cos(deg2Rad(setup.windDirection.value)),
+            setup.windSpeed.value*Math.sin(deg2Rad(setup.windDirection.value)),
+            setup.windUpdraft.value),
         new Vector3(0, 0, -9.81)
     )    
 }
