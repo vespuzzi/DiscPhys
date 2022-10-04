036a7b9ec6d5f397a4ef9cbe3b9b33bf6791229c
Vesa Kilpi <vespuzzi@gmail.com>
2022-10-05 00:51:49 +0300

    cleanup

--- src/app/simulation-chart/simulation-chart.component.ts
+++ src/app/simulation-chart/simulation-chart.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
-import { DiscProps, DiscState } from 'src/model/Disc';
-import { SimulationSetup, Environment, getInitialDiscState, initializeDiscState, Simulation, initializeEnvironment } from 'src/model/Simulator';
-import { Vector3 } from 'src/model/Vector3';
+import { DiscProps } from 'src/model/Disc';
+import { initializeDiscState, Simulation, initializeEnvironment, defaultSimulationSetup } from 'src/model/Simulator';
 
 @Component({ 
   selector: 'app-simulation-chart',
@@ -56,23 +55,17 @@ export class SimulationChartComponent implements OnInit {
         },
       }
   });
-  this.simulate(new SimulationSetup);
+  this.simulate(defaultSimulationSetup);
 };
 
-simulate(setup: SimulationSetup) : void{
+simulate(setup: any) : void{
 
     const discState =  initializeDiscState(setup);
     const env = initializeEnvironment(setup) //new Environment(1.4, new Vector3(0,0,0), new Vector3(0,0,-9.81));
-    const discProps = new DiscProps(setup.discMass/1000, setup.discDiameter/100, setup.discMassRadius/100);
+    const discProps = new DiscProps(setup.discMass.value/1000, setup.discDiameter.value/100, setup.discMassRadius.value/100);
     const simulation = new Simulation(discProps, env);
     const result = simulation.simulate([discState]);
 
-    // const mirsim  = new Simulation(discProps, env);
-    // const mirsetup = {...discSetup, spin: -discSetup.spin};
-    // const mirstate = initializeDiscState(mirsetup);
-    // const mirror = mirsim.simulate([mirstate]); 
-
-    //this.myChart?.data.datasets[0].data.push({x: this.count++, y: 70}); //= result.map(state => {return {x: state.r.x, y:state.r.y}});
     if (this.myChart) {
       this.myChart.data.datasets[0].data =  result.map(state => {return {x: state.r.x, y:state.r.y}});
       this.myChart.data.datasets[1].data =  result.map(state => {return {x: state.r.x, y:state.r.z}});
